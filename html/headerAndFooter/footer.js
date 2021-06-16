37990d692268594bed68d075211ffe9c0dd8755f
wangyl <wangyl@neusense.com>
2021-06-16 18:06:43 +0800

    nothing

---
+++ html/headerAndFooter/footer.js
@@ -0,0 +1,4 @@
+document.write('<footer class="site-footer">\n' +
+    '        \n' +
+    '        <span class="site-footer-credits"><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">此页面由</font></font><a href="https://pages.github.com"><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">GitHub Pages</font></font></a><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">生成</font><font style="vertical-align: inherit;">。</font></font></span>\n' +
+    '      </footer>')
