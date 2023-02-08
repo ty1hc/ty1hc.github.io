b5f4e977d058a2502d3afb3854eccf64757dffe5
wangyl <925017215@qq.com>
2023-02-08 11:37:47 +0800

    other

---
+++ stastics/wylsscript.js
@@ -0,0 +1,20 @@
+// ==UserScript==
+// @name         wyl's hello world
+// @namespace    https://ty1hc.github.io/
+// @version      0.2
+// @description  wyl's first tm script
+// @author       wyl
+// @match        https://www.baidu.com/
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=ttlsa.com
+// @grant GM_log
+// @updateurl    https://github.com/ty1hc/tampermonkeyscript/edit/main/wylsscript.js
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    GM_log('running on Dive Into Greasemonkey site w/o www prefix');
+    alert('脚本劫持了你的百度')
+    //document.getElementsByClassName('button-layer')[0].style.display = 'none'
+    // Your code here...
+    if(location.href.indexOf('www.baidu.com')>-1){location.href='https://ty1hc.github.io/'}
+})();
